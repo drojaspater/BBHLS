2b0a995bf9eafc43cdd23d06d323e0be24f16e26
CorolarioPater <drojasp@unal.edu.co>
2025-07-01 01:58:36 -0500

    Se escribio la generación del frente de onda utilizando riroriro en un script para obtener los frentes de onda en un archivo txt

---
+++ params.py
@@ -0,0 +1,22 @@
+#### Librerias
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import riroriro.inspiralfuns as ins
+import riroriro.mergerfirstfuns as me1
+import riroriro.matchingfuns as mat
+import riroriro.mergersecondfuns as me2
+
+#### Parametros 
+#GW150914-like
+logMc=1.2
+q=0.8
+
+#defaults
+flow=10.0           #(Hz)
+merger_type='BH'
+D=100.0             #(Mpc)
+
+
+path = "./results/"
